523a2555bf496aa50db0b8ee64c4d72a901b3b55
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-05-23 14:35:54 -0400

    Add: create class for generate error

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata'
 import 'express-async-errors'
 import express from 'express'
+import { handleError } from './errors/handle.error'
 
 const app = express()
 app.use( express.json() )
+
+app.use( handleError )
 
 export { app }
---
+++ back-end/src/errors/handle.error.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "./app.error";
+
+const handleError = async ( error: Error, req: Request, res: Response, next: NextFunction ) => {
+    if( error instanceof AppError ){
+        return res.status( error.statusCode ).json({ message: error.message })
+    }
+
+    return res.status( 500 ).json({ message: 'internal server error!' })
+}
+
+export { handleError }
