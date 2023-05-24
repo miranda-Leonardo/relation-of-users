6b0d7329dfac195d1359173cd4c29c8a59a32a4f
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-05-24 10:24:15 -0400

    Add: user route creation

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,9 +2,12 @@ import 'reflect-metadata'
 import 'express-async-errors'
 import express from 'express'
 import { handleError } from './errors/handle.error'
+import { userRoutes } from './routes/user.routes'
 
 const app = express()
 app.use( express.json() )
+
+app.use('/user', userRoutes)
 
 app.use( handleError )
 
