956af4b630d27edd0f0f373b06c463edec1e479d
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-06-02 15:43:37 -0400

    Fix: conflicts

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Context, { DataProvider } from './context/index.tsx'
+import Context from './context/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Context>
-      <App />
+      <Context>
+        <App />
+      </Context>
     </Context>
   </React.StrictMode>
 )
